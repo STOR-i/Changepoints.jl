6c84562678490e23b6a9a6212b4f16f181667b5a
Jamie Fairbrother <jfairbrother87@hotmail.com>
2014-12-16 20:37:08 +0000

    - Added omitted include statement to changepoints.jl

--- src/changepoints.jl
+++ src/changepoints.jl
@@ -3,6 +3,7 @@ module changepoints
 export PELT_general, NormalMeanChange, NormalVarSegment, NormalMeanSegment
 
 include("cost_normal_mean.jl")
+include("cost_normal_var.jl")
 include("PELT.jl")
 include("sim_type.jl")
 
