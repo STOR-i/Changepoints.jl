6e4a02bf3a71437d87c175969b00ee2d5f8d5fbe
Lawrence Bardwell <l.bardwell@lancaster.ac.uk>
2015-01-09 15:48:57 +0000

    rop update

--- src/PELT_range_of_penalties.jl
+++ src/PELT_range_of_penalties.jl
@@ -1,7 +1,7 @@
 # PELT for a range of penalties -- Make more efficient by recylcling comps , style of output?
 # see http://arxiv.org/pdf/1412.3617.pdf
 
-function PELT_range_of_penalties(segment_cost::Function , n::Int64, pen::Array(Float64,2))
+function PELT_range_of_penalties(segment_cost::Function , n::Int64, pen::Array{Float64} )
 
     pen_ranges = [ minimum(pen) , maximum(pen) ]
 
@@ -14,8 +14,8 @@ function PELT_range_of_penalties(segment_cost::Function , n::Int64, pen::Array(F
         beta1 = pen_ranges[2]
         cpts_beta0 , opt_beta0 = PELT_general( segment_cost , n , beta0 )
         cpts_beta1 , opt_beta1 = PELT_general( segment_cost , n , beta1 )
-        m0 = length(cpts_beta0)
-        m1 = length(cpts_beta1)
+        m0 = length(cpts_beta0) - 1
+        m1 = length(cpts_beta1) - 1
     
         if m0 == m1
             # same no of chpts
@@ -29,18 +29,18 @@ function PELT_range_of_penalties(segment_cost::Function , n::Int64, pen::Array(F
             # no of chpts differs by 1
             # find regions of penalty that give you required no of chpts
             # beta_int is intersection of lines
-            beta_int = ( opt_beta1 - opt_beta0 + beta0*(m0 + 1) - beta1*(m1 + 1) )/(m0 - m1)
+            beta_int = ( opt_beta1 - opt_beta0 + beta0*m0 - beta1*m1 )/(m0 - m1)
             push!( out["no of chpts"] , m0 , m1 )          
             push!( out["penalty"] , ( beta0  , beta_int ) , ( beta_int , beta1 )  )
-            push!( out["optimum"] , ( opt_beta0  , opt_beta0 + (m0 + 1)*(beta_int - beta0) ) , ( opt_beta0 + (m0 + 1)*(beta_int - beta0), opt_beta1 )   )
+            push!( out["optimum"] , ( opt_beta0  , opt_beta0 + m0*(beta_int - beta0) ) , ( opt_beta0 + m0*(beta_int - beta0), opt_beta1 )   )
             # remove first two elements
             pen_ranges = pen_ranges[3:end]
-        
+       
         else
-            beta_int = ( opt_beta1 - opt_beta0 + beta0*(m0 + 1) - beta1*(m1 + 1) )/(m0 - m1)
+            beta_int = ( opt_beta1 - opt_beta0 + beta0*m0 - beta1*m1 )/(m0 - m1)
             # do PELT at beta_int
             cpts_betaint , opt_betaint = PELT_general( segment_cost , n , beta_int )
-            mint = length(cpts_betaint)
+            mint = length(cpts_betaint) - 1
             if mint !== m1
                 push!( pen_ranges , beta0 , beta_int , beta_int , beta1 )
             end
