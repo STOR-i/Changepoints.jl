4169ea3ff917696ef248684feaf13ce1902adf5b
Lawrence Bardwell <l.bardwell@lancaster.ac.uk>
2015-02-01 22:50:23 +0000

    crops_partial

--- src/CROPS.jl
+++ src/CROPS.jl
@@ -12,25 +12,57 @@ function CROPS(segment_cost::Function , n::Int64, pen::Array{Float64} )
   
     while true
 
-        # do pelt and record no of chpts and 
+        stop = 0
+        
+        # do pelt and record no of chpts and
+        # calculates the cost of the constrained problem
+        # constrained (no of chpts = no_chpts[i])
         for i in 1:length(pen_interval)
             cpts , opt = PELT( segment_cost , n , pen_interval[i] )
 
             if ( in(length(cpts) , out_num_cpts ) )
                 # find the index
+                index = findin(out_nm_cpts,length(cpts))
                 # check for penalty is it bigger than max penalty
-            end
-            
-            # calculates the cost of the constrained problem
-            # constrained (no of chpts = no_chpts[i])
-            constrained_like[i] = opt - no_chpts[i]*pen_interval[i]
+                # index of pen_interval to delete BUT DELETED AFTER BREAKCONDITION
+                if pen_interval[b] > out_max_pen[index]
+                    # updated it
+                    ''''''
+                    ''''''
+                else
+                    # no change
+                    stop+=1
+                end
+            else
+                # penalty val has a no of chpts not been seen before
+                # first store in output
+                '''''
+                ''''
+                # then calc constrained like
+                constrained_like[i] = opt - no_chpts[i]*pen_interval[i]
+            end         
+  
         end
-        
-        # find 
-        minimum(no_chpts)
-        maximum(no_chpts)
+
+
+        sort(out_num_cpts)
+        sortperm(o)
         
         # condition to break while loop no new entries being put into out_num_cpts
+
+        # leave loop and finish subject to output
+        if stop == length(pen_interval)
+            break
+        end
+
+        # NOWdelete items from pen_interval
+        # look through out_num_cpts and calc beta_int and put these in pen_interval
+        for i in 1:length(out_num_cpts)
+
+            out_num_cpts[i]  out_num_cpts[i+1]
+
+        end
+        
         
     end
 
