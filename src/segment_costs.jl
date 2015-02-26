1e1c8f573dcb719680bd8f7646d6df849f6ea0b5
Kaylea <k.haynes1@lancaster.ac.uk>
2015-02-26 14:56:47 +0000

    indices ammended to match R package

--- src/segment_costs.jl
+++ src/segment_costs.jl
@@ -1,35 +1,35 @@
 function NormalMeanSegment(data::Array{Float64})
     cd = [0,cumsum( data )]
     cd_2 = [0,cumsum( abs2(data) )]
-    cost(s::Int64, t::Int64) = cd_2[t+1] - cd_2[s] - abs2(cd[t+1] - cd[s])/(t-s+1)
+    cost(s::Int64, t::Int64) = cd_2[t+1] - cd_2[s+1] - abs2(cd[t+1] - cd[s+1])/(t-s)
     return cost
 end
 
 function NormalVarSegment(data::Array{Float64}, μ::Float64)
     ss = [0,cumsum((data - μ).^2)]
-    cost(s::Int64, t::Int64) = (t-s+1) * log( (ss[t+1] - ss[s])/(t-s+1) ) 
+    cost(s::Int64, t::Int64) = (t-s) * log( (ss[t+1] - ss[s+1])/(t-s) ) 
 end
 
 function NormalMeanVarSegment(data::Array{Float64})
     cd = [0,cumsum( data )]
     cd_2 = [0,cumsum( abs2(data) )]
     function cost(s::Int64, t::Int64) 
-      mu = (cd[t+1] - cd[s])/(t-s+1)
-      sig = ( cd_2[t+1] - cd_2[s] )/(t-s+1) - mu^2
-      return (t-s+1)*log(sig) + (cd_2[t+1] - cd_2[s])/sig - 2*(cd[t+1] - cd[s])*mu/sig + ((t-s+1)*mu^2)/sig
+      mu = (cd[t+1] - cd[s+1])/(t-s)
+      sig = ( cd_2[t+1] - cd_2[s+1] )/(t-s) - mu^2
+      return (t-s)*log(sig) + (cd_2[t+1] - cd_2[s+1])/sig - 2*(cd[t+1] - cd[s+1])*mu/sig + ((t-s)*mu^2)/sig
     end
     return cost
 end
 
 function ExponentialSegment(data::Array{Float64})
     cd = [0,cumsum( data )]
-    cost(s::Int64, t::Int64) = -2*(t-s+1) * ( log(t-s+1) - log(cd[t+1] - cd[s]) - 1 )  
+    cost(s::Int64, t::Int64) = -2*(t-s) * ( log(t-s) - log(cd[t+1] - cd[s+1]) - 1 )  
     return cost
 end
 
 function PoissonSegment(data::Array{Int64})
     cd = [0,cumsum( data )]
-    cost(s::Int64, t::Int64) = -2*(cd[t+1]-cd[s]) * ( log(cd[t+1]-cd[s]) - log(t-s+1) - 1 )
+    cost(s::Int64, t::Int64) = -2*(cd[t+1]-cd[s+1]) * ( log(cd[t+1]-cd[s+1]) - log(t-s) - 1 )
     return cost
 end
 
@@ -40,11 +40,11 @@ function BetaSegment(data::Array{Float64})
     lcd = [0,cumsum( log(data) )]
     clcd = [0,cumsum( log(1-data) )]
     function cost(s::Int64, t::Int64) 
-      mu = (cd[t+1] - cd[s])/(t-s+1)
-      sig = ( cd_2[t+1] - cd_2[s] )/(t-s+1) - mu^2
+      mu = (cd[t+1] - cd[s+1])/(t-s)
+      sig = ( cd_2[t+1] - cd_2[s+1] )/(t-s) - mu^2
       alpha = mu*( mu*(1-mu)/sig - 1 )
       beta = (1-mu)*( mu*(1-mu)/sig - 1 )
-      return (t-s+1)*lbeta(alpha,beta) - (alpha-1)*( lcd[t+1]-lcd[s] ) - (beta-1)*( clcd[t+1]-clcd[s] )
+      return (t-s)*lbeta(alpha,beta) - (alpha-1)*( lcd[t+1]-lcd[s+1] ) - (beta-1)*( clcd[t+1]-clcd[s+1] )
     end
     return cost
 end
