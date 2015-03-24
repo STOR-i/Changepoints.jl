58fb1546dec5b89bbf7c156d50167ed6a9a812ca
Kaylea <k.haynes1@lancaster.ac.uk>
2015-03-24 15:53:19 +0000

    Gamma Cpts edited

--- src/segment_costs.jl
+++ src/segment_costs.jl
@@ -40,7 +40,7 @@ end
 
 function ExponentialSegment(data::Array{Float64})
     cd = [0,cumsum( data )]
-    cost(s::Int64, t::Int64) = -2*(t-s) * ( log(t-s) - log(cd[t+1] - cd[s+1]) - 1 )  
+    cost(s::Int64, t::Int64) = -*(t-s) * ( log(t-s) - log(cd[t+1] - cd[s+1]))  
     return cost
 end
 
