15fbcd1efb96ae2ad69e3a464d3005a3017867ea
Kaylea <k.haynes1@lancaster.ac.uk>
2015-04-15 16:23:05 +0100

    CROPS now matches R

--- src/CROPS.jl
+++ src/CROPS.jl
@@ -9,7 +9,7 @@ function CROPS(segment_cost::Function , n::Int64, pen::(Real,Real) )
     out_num_cpts = Array(Int64,0)
     out_max_pen = Array(Float64,0)
     out_constrain = Array(Float64,0)
-    out_cpts = Array(Array{Int64},0) #Kaylea added
+    out_cpts = Array(Array{Int64},0) 
    
     while true
 
@@ -23,15 +23,12 @@ function CROPS(segment_cost::Function , n::Int64, pen::(Real,Real) )
 
                 # do PELT for each pen_interval
                 cpts , opt = PELT( segment_cost , n , pen=pen_interval[i] )
-
+               
                 # if there are already same no of chpts in out 
                 # see if penalty differentx
                 if ( in(  length(cpts) , out_num_cpts ) )
-                    
-                    # find the index #Kaylea added [1] at end of index for it to work 
                     index = findin(out_num_cpts,length(cpts))[1]
                  
-                    
                     # check for penalty is it bigger than max penalty
                     if pen_interval[i] > out_max_pen[index]
                         out_max_pen[index] = pen_interval[i]
@@ -44,9 +41,9 @@ function CROPS(segment_cost::Function , n::Int64, pen::(Real,Real) )
                     # number of chpts not seen before add to output and constrained likelihood
 
                     push!( out_num_cpts , length(cpts) )
-                    push!( out_constrain , opt - length(cpts)*pen_interval[i] )
+                    push!( out_constrain , opt - (length(cpts)-1)*pen_interval[i] )
                     push!( out_max_pen , pen_interval[i])
-                    push!( out_cpts, cpts) #Kaylea added
+                    push!( out_cpts, cpts) 
             
                    
                 end         
@@ -66,12 +63,12 @@ function CROPS(segment_cost::Function , n::Int64, pen::(Real,Real) )
         out_num_cpts = out_num_cpts[ord_ind]
         out_constrain = out_constrain[ord_ind]
         out_max_pen = out_max_pen[ord_ind]
-        out_cpts = out_cpts[ord_ind] #Kaylea added
+        out_cpts = out_cpts[ord_ind]
         
         # make pen_interval 
-        pen_interval = Array(Float64,0) #Kaylea changed the type of pen_interval 
+        pen_interval = Array(Float64,0)
         
-        for i in 1:(length(out_num_cpts)-1) #Kayea changed to -1
+        for i in 1:(length(out_num_cpts)-1) 
 
             # if they differ by 1, then just calc beta_int and extend range of pen for large no of chpts
             if out_num_cpts[i] ==  out_num_cpts[i+1] + 1
@@ -85,12 +82,11 @@ function CROPS(segment_cost::Function , n::Int64, pen::(Real,Real) )
             
         end
 
-        # Kaylea added this part in to stop weird results 
        if length(pen_interval) > 0
            i = 1
            while i <= length(pen_interval)
                for j in 1:length(out_max_pen) 
-                   if abs(pen_interval[i] - out_max_pen[j]) < 1e-1
+                   if abs(pen_interval[i] - out_max_pen[j]) < 1e-2
                        splice!(pen_interval,i)
                        i = i - 1
                        break
@@ -105,10 +101,37 @@ function CROPS(segment_cost::Function , n::Int64, pen::(Real,Real) )
     # end of while loop    
     end
 
+    # Calculate beta intervals
+    nb = length(out_max_pen)
+    beta_e= Array(Float64,0)
+    beta_int = Array(Float64,0)
+    sort_out_max_pen = sort(out_max_pen);
+    sort_out_constrain = sort(out_constrain);
+    sort_out_num_cpts = sort(out_num_cpts, rev = true);
+    
+    for k in 1:nb
+        if k == 1
+            beta_int = push!(beta_int, sort_out_max_pen[1])
+        
+        else 
+            beta_int = push!(beta_int, beta_e[k-1])
+        end
+        
+   
+        if k == nb
+            beta_e = push!(sort_out_max_pen[k])
+   
+        else
+            beta_e = push!(beta_e,(sort_out_constrain[k] - sort_out_constrain[k+1])/(sort_out_num_cpts[k+1] - sort_out_num_cpts[k]))
+        end
+    end
+    
+    
+        
     # organise output into a dictionary
     out = Dict{ASCIIString, Array}()
     out["number"] = out_num_cpts
-    out["penalty"] = out_max_pen
+    out["penalty"] = sort(beta_int, rev = true)
     out["constrained"] = out_constrain
     out["changepoints"] = out_cpts
     return out
