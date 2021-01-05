233ed4f366d2fb7436a18ae1c7c36b9acc8d76c5
Dom Owens <dom.owens@bristol.ac.uk>
2021-01-05 17:34:14 +0000

    seeded binary segmentation

--- src/WBS.jl
+++ src/WBS.jl
@@ -24,6 +24,7 @@ WBS_cps, WBS_cost = WBS(seg_cost, n)
 
 # References
 Fryzlewicz, P. (2014) Wild binary segmentation for multiple change-point detection, Annals of Statistics 42(6), 2243-2281
+Kovács, S., Li, H., Bühlmann, P., & Munk, A. (2020). Seeded Binary Segmentation: A general methodology for fast and optimal change point detection. arXiv preprint arXiv:2002.06633.
 """
 #import StatsBase
 #using StatsBase
@@ -31,14 +32,15 @@ Fryzlewicz, P. (2014) Wild binary segmentation for multiple change-point detecti
 result_type = @NamedTuple{s::Int64, e::Int64, cpt::Int64, CUSUM::Float64, min_th::Float64, scale::Int64}
 
 
-function WBS( segment_cost::Function , n::Int64, th_const::Float64 = 1.3, sigma::Float64 = 1.0, M::Int64 = 5000)
+function WBS( segment_cost::Function , n::Int64, th_const::Float64 = 1.3, sigma::Float64 = 1.0,
+    M::Int64 = 5000, do_seeded::Bool = false, shrink::Float64 = 1/sqrt(2))
     result = Array{result_type}(undef,0)
 
     # thresulthold
     th = sigma * th_const * sqrt(2 * log(n))
     scale = 0
     # call WBS
-    result = WBS_RECUR(segment_cost, 1, n, th, result, scale, M)
+    result = WBS_RECUR(segment_cost, 1, n, th, result, scale, M, do_seeded, shrink)
 
 
     return result
@@ -46,25 +48,30 @@ end
 
 
 function WBS_RECUR(segment_cost::Function, s::Int64, e::Int64, th::Float64,
-    result::Array{result_type}, scale::Int64, M::Int64 = 5000)
+    result::Array{result_type}, scale::Int64, M::Int64 = 5000, do_seeded::Bool = false, shrink::Float64 = 1/sqrt(2))
     #k = length(CP) - 2 #current number of change points
-    minlength = 30 #minimum interval length
-    tau = Tuple{Int, Int}[] # Segmentations to test
-    push!(tau, (s, e))
+    minlength = 20 #minimum interval length
     scale += 1 #update scale
 
     # draw intervals
-    for m in 2:M
-        s_m = rand(s:(e-minlength))
-        e_m = rand( (s_m+minlength):e)
-        push!(tau, (s_m, e_m))
+    if do_seeded
+        tau = get_seeded_intervals(s,e, shrink, minlength)
+        M = length(tau)
+    else
+        tau = Tuple{Int, Int}[] # Segmentations to test
+        push!(tau, (s, e))
+        for m in 2:M
+            s_m = rand(s:(e-minlength))
+            e_m = rand( (s_m+minlength):e)
+            push!(tau, (s_m, e_m))
+        end
     end
 
     # compute CUSUMs find candidate change point
     x = fill(0.0,M, e-s+1)#zeros(Float64, M, e-s+1)#Array{Float64}(NaN, M, e-s+1 ) #CUSUM array
     for m in 1:M
         a, b = pop!(tau)
-        for j in (a + 1):(b - 1)
+        for j in (a + 1):(b - 2)
             x[m,j-s+1] = segment_cost(s,e,j)
         end
     end
@@ -72,17 +79,17 @@ function WBS_RECUR(segment_cost::Function, s::Int64, e::Int64, th::Float64,
     maxval , ind = findmax(x)
 
     # segment here
-    if maxval > th
+    if maxval > th #&& scale < 10
         cpt = ind[2] + s -1
         newresult = (s=s,e=e, cpt=cpt, CUSUM = maxval, min_th=th, scale=scale )
         push!(result, newresult)
         #push!(CP,cpt )
         #push!(CUSUM, minval)
-        if cpt - s > minlength
-            result = WBS_RECUR(segment_cost, s, cpt, th, result, scale, M)
+        if cpt - s > 2*minlength
+            result = WBS_RECUR(segment_cost, s, cpt, th, result, scale, M, do_seeded, shrink)
         end
-        if e - cpt > minlength
-            result = WBS_RECUR(segment_cost, cpt+1, e, th, result,  scale, M)
+        if e - cpt > 2*minlength
+            result = WBS_RECUR(segment_cost, cpt+1, e, th, result,  scale, M, do_seeded, shrink)
         end
     end
     #CP = sort(CP)
@@ -92,6 +99,7 @@ end
 
 function get_WBS_changepoints(segment_cost::Function, object::Array{NamedTuple{(:s, :e, :cpt, :CUSUM, :min_th, :scale),Tuple{Int64,Int64,Int64,Float64,Float64,Int64}},1}, Kmax::Int64 = 1,  alpha::Float64 = 1.01)
     #result = Array{result_type}(undef,0)
+    Kmax = min(Kmax,length(object))
     if length(object) > 0
         cost = zeros(Kmax+1)
         sorted = sort(object, lt = (a, b) -> ( a[4] < b[4]) ) #a[6] < b[6] &&
@@ -115,9 +123,29 @@ function get_WBS_changepoints(segment_cost::Function, object::Array{NamedTuple{(
         optimal = findmin(cost)[2] -1 #optimal number
         #out_cps = sort(object[1:(optimal+2)], lt = (a, b) -> (a[1][2] < b[1][2]) )
 
-        return  optimal , cost
+        return  optimal , cost, object[1:optimal]
     else
          println("no changepoints detected")
     end
 
+end
+
+function get_seeded_intervals(s::Int64, e::Int64,  shrink::Float64 = 1/sqrt(2), minlength = 10)
+    n = e-s+1;
+    max_k = Int(ceil(log(1/shrink, float(n) )))
+    out = Tuple{Int, Int}[]
+
+    for k = 1:max_k
+        n_k = Int(2 * ceil(shrink^(1-k) ) - 1)
+        l_k = n*shrink^(k-1)
+        s_k = (n-l_k)/(n_k -1)
+        if !isnan(s_k) && l_k >= minlength
+            for i in 1:n_k
+                left =  s + Int( floor((i-1)*s_k) )
+                right = s + Int( ceil((i-1)*s_k + l_k) ) -1
+                push!(out, (left, right) )
+            end
+        end
+    end
+    return out
 end
